92a31da19a51ffd9161f1ae7764072fd30149104
Nikhil Bhosale <nikhil.bhosale@voiapp.io>
2024-06-01 13:01:30 +0200

    Created new project

---
+++ eDictionary/eDictionary/eDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  eDictionaryApp.swift
+//  eDictionary
+//
+//  Created by Nikhil Bhosale on 2024-06-01.
+//
+
+import SwiftUI
+
+@main
+struct eDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ eDictionary/eDictionaryUITests/eDictionaryUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  eDictionaryUITestsLaunchTests.swift
+//  eDictionaryUITests
+//
+//  Created by Nikhil Bhosale on 2024-06-01.
+//
+
+import XCTest
+
+final class eDictionaryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
